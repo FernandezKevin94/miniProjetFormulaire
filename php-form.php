0be9397ee271bbb8b2ac17638ac4565ba9d63334
Yanis <say.yanis91@gmail.com>
2024-12-20 13:58:10 +0100

    table client

--- php-form.php
+++ php-form.php
@@ -1,2 +1,24 @@
 <?php
 
+include "cnxConfig.php";
+
+$bdd = "create table Client(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    civilite ENUM('M.', 'Mme') NOT NULL,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    dateNaissance DATE NOT NULL,
+    communeNaissance VARCHAR(100) NOT NULL,
+    telephone VARCHAR(15),
+    courriel VARCHAR(100),
+    siteWeb VARCHAR(255),
+    niveauAnglais ENUM('A1', 'B1', 'C1') NOT NULL,
+    html BOOLEAN DEFAULT FALSE,
+    css BOOLEAN DEFAULT FALSE,
+    javascript BOOLEAN DEFAULT FALSE,
+    php BOOLEAN DEFAULT FALSE
+    )";
+
+// Exécution de la requête
+$connexion->exec($bdd);
+echo "Table Client created successfully";
